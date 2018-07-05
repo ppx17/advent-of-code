59a0ccf402b78b050ee50ed0b79e2b33361fd378
Niels <niels@bizqit.nl>
2018-07-05 08:51:00 +0200

    Add day 18

---
+++ day18-part2.ps1
@@ -0,0 +1,156 @@
+[CmdletBinding()]
+Param(
+    [string]$InputFile = 'input-day18.txt'
+);
+
+Class AssemblyRegistry {
+    [System.Collections.Hashtable]$Registry = [System.Collections.Hashtable]@{}
+
+    [Int64]Get([string]$Name) {
+        if($null -eq $this.Registry[$Name]) {
+            return 0;
+        }
+        return $this.Registry[$Name];
+    }
+
+    [void]Set([string]$Name, [string]$NameOrValue) {
+        $this.Registry[$Name] = $this.ResolveNameOrValue($NameOrValue);
+    }
+
+    [void]Add([string]$Name, [string]$NameOrValue) {
+        $this.Set($Name, ($this.Get($Name) + $this.ResolveNameOrValue($NameOrValue)));
+    }
+
+    [void]Mul([string]$Name1,  [string]$NameOrValue) {
+        $this.Set([string]$Name1, ($this.Get($Name1) * $this.ResolveNameOrValue($NameOrValue)));
+    }
+
+    [void]Mod([string]$Name, [string]$NameOrValue) {
+        $this.Set([string]$Name, ($this.Get($Name) % $this.ResolveNameOrValue($NameOrValue)));
+    }
+
+    [Int64]ResolveNameOrValue([string]$NameOrValue) {
+        if($NameOrValue -match "^[a-z]+$") {
+            return $this.Get($NameOrValue);
+        }
+        return $NameOrValue;
+    }
+}
+
+Class AssemblyInterpreter {
+
+    [int]$LastFrequency;
+    [AssemblyRegistry]$Registry;
+    [array]$Instructions;
+    [int]$NextInstruction;
+    [bool]$IsRunning;
+    [bool]$IsWaitingForMessage;
+    [int]$ProcessId;
+    [System.Collections.Queue]$ReceiveQueue;
+
+    AssemblyInterpreter([array]$Instructions, [int]$ProcessId) {
+        $this.Instructions = $Instructions;
+        $this.Registry = [AssemblyRegistry]::new();
+        $this.Registry.Set("p", $ProcessId);
+        $this.ProcessId = $ProcessId;
+        $this.IsRunning = $true;
+        $this.IsWaitingForMessage = $false;
+        $this.ReceiveQueue = [System.Collections.Queue]::new();
+    }
+
+    [bool]IsValidInstruction($InX) {
+        return $InX -ge 0 -and $InX -lt $this.Instructions.Count;
+    }
+
+    [object]RunInstruction($InX) {
+        if( -not $this.IsValidInstruction($InX)) {
+            # End of program reached, terminate
+            Write-Verbose ("#{0}: Terminated because end of code is reached" -f $this.ProcessId);
+            $this.Terminate();
+            return $null;
+        }
+        $Send = $null;
+        $Instruction = $this.GetInstruction($InX);
+        $Jump = 1;
+        switch($Instruction[0]) {
+            "snd" { $Send = $this.Registry.Get($Instruction[1]); }
+            "set" { $this.Registry.Set($Instruction[1], $Instruction[2]); }
+            "add" { $this.Registry.Add($Instruction[1], $Instruction[2]); }
+            "mul" { $this.Registry.Mul($Instruction[1], $Instruction[2]); }
+            "mod" { $this.Registry.Mod($Instruction[1], $Instruction[2]); }
+            "rcv" { 
+                if($this.ReceiveQueue.Count -eq 0) {
+                    # Deadlock
+                    $this.IsWaitingForMessage = $true;
+                    return $null;
+                }
+                $this.Registry.Set($Instruction[1], $this.ReceiveQueue.Dequeue());
+             }
+            "jgz" { 
+                $X = $this.Registry.ResolveNameOrValue($Instruction[1]);
+                $Y = $this.Registry.ResolveNameOrValue($Instruction[2]);
+                if( $X -gt 0) { 
+                    $Jump = $Y;
+                }
+            }
+        }
+        $this.NextInstruction = $InX + $Jump;
+
+        return $Send;
+    }
+
+    [object]RunNextInstruction() {
+        if($this.IsRunning -ne $true) {
+            Write-Error "Running instruction for terminated program.";
+            return $null;
+        }
+        return $this.RunInstruction($this.NextInstruction);
+    }
+
+    [void]ReceiveMessage([Int64]$Message) {
+        $this.IsWaitingForMessage = $false;
+        $this.ReceiveQueue.Enqueue($Message);
+    }
+
+    [array]GetInstruction($InX) {
+        Write-Verbose ("#{0}: ({1}) {2}" -f $this.ProcessId, $InX, $this.Instructions[$InX]);
+        return ($this.Instructions[$InX] -Split " ");
+    }
+
+    [void]Terminate() {
+        $this.IsRunning = $false;
+    }
+}
+
+$Instructions = Get-Content $InputFile;
+$Instance0 = [AssemblyInterpreter]::new($Instructions, 0);
+$Instance1 = [AssemblyInterpreter]::new($Instructions, 1);
+
+$MessagesSentInstance1 = 0;
+while($Instance1.IsRunning) {
+    # We only continue while instance 1 is running, since our goal is to count the snd messages executed by instance 1.
+
+    if($Instance0.IsRunning -and -not $Instance0.IsWaitingForMessage) {
+        $Message = $Instance0.RunNextInstruction();
+        if($Message -ne $null) {
+            Write-Verbose ("Sending message '{0}' from {1} to {2}" -f $Message, $Instance0.ProcessId, $Instance1.ProcessId);
+            $Instance1.ReceiveMessage($Message);
+        }
+    }
+    if($Instance1.IsRunning -and -not $Instance1.IsWaitingForMessage) {
+        $Message = $Instance1.RunNextInstruction();
+        if($Message -ne $null) {
+            $MessagesSentInstance1++;
+            Write-Verbose ("Sending message '{0}' from {1} to {2}" -f $Message, $Instance1.ProcessId, $Instance0.ProcessId);
+            $Instance0.ReceiveMessage($Message);
+        }
+    }
+    if($Instance0.IsWaitingForMessage -and $Instance1.IsWaitingForMessage) {
+        Write-Verbose "Deadlock detected, terminating both programs";
+        $Instance0.Terminate();
+        $Instance1.Terminate();
+    }
+}
+
+
+Write-Output $MessagesSentInstance1;
---
+++ day18.ps1
@@ -0,0 +1,97 @@
+[CmdletBinding()]
+Param(
+    [string]$InputFile = 'input-day18.txt'
+);
+
+Class AssemblyRegistry {
+    [System.Collections.Hashtable]$Registry = [System.Collections.Hashtable]@{}
+
+    [Int64]Get([string]$Name) {
+        if($null -eq $this.Registry[$Name]) {
+            return 0;
+        }
+        return $this.Registry[$Name];
+    }
+
+    [void]Set([string]$Name, [string]$NameOrValue) {
+        $this.Registry[$Name] = $this.ResolveNameOrValue($NameOrValue);
+    }
+
+    [void]Add([string]$Name, [string]$NameOrValue) {
+        $this.Set($Name, ($this.Get($Name) + $this.ResolveNameOrValue($NameOrValue)));
+    }
+
+    [void]Mul([string]$Name1,  [string]$NameOrValue) {
+        $this.Set([string]$Name1, ($this.Get($Name1) * $this.ResolveNameOrValue($NameOrValue)));
+    }
+
+    [void]Mod([string]$Name, [string]$NameOrValue) {
+        $this.Set([string]$Name, ($this.Get($Name) % $this.ResolveNameOrValue($NameOrValue)));
+    }
+
+    [Int64]ResolveNameOrValue([string]$NameOrValue) {
+        if($NameOrValue -match "^[a-z]+$") {
+            return $this.Get($NameOrValue);
+        }
+        return $NameOrValue;
+    }
+}
+
+Class AssemblyInterpreter {
+
+    [int]$LastFrequency;
+    [AssemblyRegistry]$Registry;
+    [array]$Instructions;
+    [int]$CurrentInstruction;
+
+    AssemblyInterpreter([array]$Instructions) {
+        $this.Instructions = $Instructions;
+        $this.CurrentInstruction = 0;
+        $this.Registry = [AssemblyRegistry]::new();
+    }
+
+    [void]Run() {
+        $this.RunInstruction(0);
+    }
+
+    [bool]IsValidInstruction($InX) {
+        return $InX -ge 0 -and $InX -lt $this.Instructions.Count;
+    }
+
+    [void]RunInstruction($InX) {
+        if( -not $this.IsValidInstruction($InX)) {
+            # End of program reached, terminate
+            return;
+        }
+
+        $Instruction = $this.GetInstruction($InX);
+        $Jump = 1;
+        switch($Instruction[0]) {
+            "snd" { $this.LastFrequency = $this.Registry.Get($Instruction[1]); }
+            "set" { $this.Registry.Set($Instruction[1], $Instruction[2]); }
+            "add" { $this.Registry.Add($Instruction[1], $Instruction[2]); }
+            "mul" { $this.Registry.Mul($Instruction[1], $Instruction[2]); }
+            "mod" { $this.Registry.Mod($Instruction[1], $Instruction[2]); }
+            "rcv" { if($this.Registry.Get($Instruction[1]) -gt 0) { return; } }
+            "jgz" { 
+                if( $this.Registry.Get($Instruction[1]) -gt 0) { 
+                    $Jump = [int]$Instruction[2];
+                }
+            }
+        }
+        $this.RunInstruction($InX + $Jump);
+    }
+
+    [array]GetInstruction($InX) {
+        return ($this.Instructions[$InX] -Split " ");
+    }
+
+
+}
+
+
+$Instructions = Get-Content $InputFile;
+$Interpreter = [AssemblyInterpreter]::new($Instructions);
+$Interpreter.Run();
+
+Write-Output $Interpreter.LastFrequency;
