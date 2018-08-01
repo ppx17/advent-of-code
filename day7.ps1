a19dc9f4f33ac931c4fecd8d8d69191faf805dad
Niels Oostindiër <niels@bizqit.local>
2018-08-01 13:47:08 +0200

    Fix day7 part2

--- day7.ps1
+++ day7.ps1
@@ -1,10 +1,15 @@
+[CmdletBinding()]
+Param(
+	[string]$InputFile = 'input-day7.txt'
+);
+
 Class Node {
 	[String]$Parent;
 	[String]$Name;
 	[int]$Weight;
 	[System.Collections.ArrayList]$ChildrenNames = @();
 	[System.Collections.ArrayList]$Children = @();
-		
+
 	Node([string]$Name, [string[]]$ChildrenNames, $Weight) {
 		$this.Name = $Name;
         $this.Weight = $Weight;
@@ -12,19 +17,19 @@ Class Node {
 		    $this.ChildrenNames.AddRange($ChildrenNames);
         }
 	}
-	
+
 	[void]SetParent([string]$ParentName) {
 		$this.Parent = $ParentName;
-	}	
+	}
 
     [int]TowerWeight() {
         $w = $this.Weight;
-        $this.Children | % { $w += $_.Weight }
+        $this.Children | ForEach-Object { $w += $_.TowerWeight(); }
         return $w;
     }
 
     [int[]] ChildrenTowerWeights() {
-        return $this.Children | %{ $_.TowerWeight() };
+        return $this.Children | ForEach-Object { $_.TowerWeight() };
     }
 
     [bool]IsBalanced() {
@@ -44,36 +49,36 @@ Class Node {
 
 Class Tree {
 	[System.Collections.HashTable]$Nodes = @{};
-	
+
 	[void] AddNode([string]$Name, [string[]]$ChildrenNames, $Weight) {
-		
+
 		$Node = [Node]::new($Name, $ChildrenNames, $Weight);
-		
+
 		$this.Nodes.Add($Name, $Node);
-		
+
 		# See if children of this node are already in the tree
         if($ChildrenNames.Length -ne 0) {
-		    $ChildrenNames | % {
+		    $ChildrenNames | ForEach-Object {
 			    $Child = $this.Nodes.Item($_);
-			    if($Child -ne $null) {
+			    if($null -ne $Child) {
 				    $Child.SetParent($Name);
 			    }
 		    };
         }
-				
+
 		# See if the parent of this node is already in the tree
-		$this.Nodes.GetEnumerator() | % {
+		$this.Nodes.GetEnumerator() | ForEach-Object {
 			if($_.Value.ChildrenNames.IndexOf($Name) -ne -1) {
 				$Node.SetParent($_.Value.Name);
 				#break;
 			}
 		};
 	}
-	
+
 	[Node] FindRoot() {
-		
-		$RootCandidates = $this.Nodes.Values | ? Parent -eq $null;
-		
+
+		$RootCandidates = $this.Nodes.Values | Where-Object Parent -eq $null;
+
 		if($RootCandidates.Length -eq 1) {
 			return $RootCandidates[0];
 		}else{
@@ -82,25 +87,25 @@ Class Tree {
 	}
 
     [void] GiveNodesChildEntities() {
-        $this.Nodes.GetEnumerator() | % {
-            $Children = $($_.Value.ChildrenNames | % { $this.Nodes[$_] });
+        $this.Nodes.GetEnumerator() | ForEach-Object {
+            $Children = $($_.Value.ChildrenNames | ForEach-Object { $this.Nodes[$_] });
             if($Children.Length -gt 0) {
                 $_.Value.Children.AddRange($Children);
             }
         }
     }
-		
+
 }
 
 $Tree = [Tree]::new();
 
 
-(gc 'input-day7.txt') | Select-String -Pattern "^(?<Name>[a-z]+)\s+\((?<Weight>[0-9]+)\)(\s+\-\>\s+(?<Children>[a-z, ]+))?$" | % {
-	$Name = $($_.Matches.Groups | ? Name -eq "Name").Value;
-	$Weight = $($_.Matches.Groups | ? Name -eq "Weight").Value
-	$RawChildren = $($_.Matches.Groups | ? Name -eq "Children").Value
-	
-	$Tree.AddNode($Name, $($RawChildren.Split(", ") | ? { $_ -ne ""} ), [convert]::ToInt32($Weight, 10));
+(Get-Content $InputFile) | Select-String -Pattern "^(?<Name>[a-z]+)\s+\((?<Weight>[0-9]+)\)(\s+\-\>\s+(?<Children>[a-z, ]+))?$" | ForEach-Object {
+	$Name = $($_.Matches.Groups | Where-Object Name -eq "Name").Value;
+	$Weight = $($_.Matches.Groups | Where-Object Name -eq "Weight").Value
+	$RawChildren = $($_.Matches.Groups | Where-Object Name -eq "Children").Value
+
+	$Tree.AddNode($Name, $($RawChildren.Split(", ") | Where-Object { $_ -ne ""} ), [convert]::ToInt32($Weight, 10));
 }
 
 $RootNode = $Tree.FindRoot();
@@ -121,7 +126,7 @@ Function Test-Subtree {
     }
 
     # If one of my children is inbalanced, he must fix the problem
-    $Node.Children | % {
+    $Node.Children | ForEach-Object {
         if(-not $_.IsBalanced()) {
             Test-Subtree -Node $_;
             return;
@@ -131,9 +136,9 @@ Function Test-Subtree {
     # I'm the only one who can see the balance problem, find odd one out
     $Weights = $Node.ChildrenTowerWeights();
 
-    $Mode = [convert]::ToInt32($($Weights | group | sort -Descending count | Select -First 1).Name, 10);
+    $Mode = [convert]::ToInt32($($Weights | Group-Object | Sort-Object -Descending count | Select-Object -First 1).Name, 10);
 
-    $ProblematicChild = $($Node.Children | ? {$_.TowerWeight() -ne $Mode})[0];
+    $ProblematicChild = $($Node.Children | Where-Object {$_.TowerWeight() -ne $Mode})[0];
 
     $Difference = $ProblematicChild.TowerWeight() - $Mode;
 
@@ -141,6 +146,3 @@ Function Test-Subtree {
 }
 
 Test-Subtree $RootNode;
-
-
-        
