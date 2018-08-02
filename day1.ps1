8f72ba63defffe961e066b18e653f66cbf3e95a8
Niels Oostindiër <niels@bizqit.local>
2018-08-02 10:01:37 +0200

    Small cleanup for day 1

--- day1.ps1
+++ day1.ps1
@@ -1,16 +1,14 @@
 Param(
-	[string]$Str
+	[string]$InputFile = 'input-day1.txt'
 );
-$Chars = $Str.ToCharArray()
-for($i,$y=0;$i -lt $Chars.Length;$i++) { 
+$Chars = (Get-Content $InputFile -Raw).ToCharArray() | ForEach-Object { [Convert]::ToInt32($_, 10) };
+for($i,$y,$y2=0;$i -lt $Chars.Length;$i++) { 
 	if($Chars[($i + 1) % $Chars.Length] -eq $Chars[$i]) {
-		$y += ([convert]::ToInt32($Chars[$i], 10)); 
+		$y += $Chars[$i];
 	}
-}
-return $y;
-
-for($i,$y=0;$i -lt $Chars.Length;$i++) { 
 	if($Chars[($i + ($Chars.Length / 2)) % $Chars.Length] -eq $Chars[$i]) {
-		$y += ([convert]::ToInt32($Chars[$i], 10)); 
+		$y2 += $Chars[$i];
 	}
 }
+Write-Output "Part 1: ${y}";
+Write-Output "Part 2: ${y2}";
