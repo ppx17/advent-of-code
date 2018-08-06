22feba5974714f48a8042d02acf864199ec81c70
Niels Oostindiër <niels@bizqit.local>
2018-08-06 08:48:27 +0200

    Clean-up for day 2

--- day2.ps1
+++ day2.ps1
@@ -1,11 +1,17 @@
-$Content = Get-Content "input-day2-lytse.txt";
+Param(
+	[string]$InputFile = 'input-day2.txt'
+);
+$Content = Get-Content $InputFile;
 $sum=0;
-$Content | %{ $h=0;$l=[int]::MaxValue; $_ -Split '\s+' | % { $x = [convert]::ToInt32($_, 10);$h=[math]::max($x,$h);$l=[math]::min($x,$l); }; $sum += ($h-$l); }
-#return $sum;
+$Content | ForEach-Object { $h=0;$l=[int]::MaxValue; $_ -Split '\s+' | ForEach-Object { $x = [convert]::ToInt32($_, 10);$h=[math]::max($x,$h);$l=[math]::min($x,$l); }; $sum += ($h-$l); }
+Write-Output "Part 1 methdo 1: ${sum}";
+
+$sum = 0; $Content | ForEach-Object { $Stat = [int[]]($_ -Split '\s+') | Measure-Object -Maximum -Minimum; $sum += ($Stat.Maximum - $Stat.Minimum); }
+Write-Output "Part 1 method 2: ${sum}";
 
 $sum=0;
-$Content | %{ 
-	$digits = $_ -Split '\s+' | % { [convert]::ToInt32($_, 10);	}; 
+$Content | ForEach-Object{ 
+	$digits = $_ -Split '\s+' | ForEach-Object { [convert]::ToInt32($_, 10); }; 
 	for($x=0;$x -lt $digits.length;$x++) {
 		for($y=$x+1;$y -lt $digits.length;$y++) {
 			if($digits[$x] % $digits[$y] -eq 0) {
@@ -17,11 +23,11 @@ $Content | %{
 	}
 }
 
-Write-Host $sum;
+Write-Host "Part 2 method 1: ${sum}";
 
 $sum=0;
-$Content | %{ 
-	$digits = $_ -Split '\s+' | % { [convert]::ToInt32($_, 10);	}; 
+$Content | ForEach-Object{ 
+	$digits = $_ -Split '\s+' | ForEach-Object { [convert]::ToInt32($_, 10); }; 
 	for($x=0;$x -lt $digits.length;$x++) {
 		for($y=0;$y -lt $digits.length;$y++) {
 			if($x -eq $y) { continue; }
@@ -32,11 +38,11 @@ $Content | %{
 	}
 }
 
-Write-Host $sum;
+Write-Host "Part 2 method 2: ${sum}";
 
 $sum=0;
-$Content | %{ 
-	$digits = $_ -Split '\s+' | % { [convert]::ToInt32($_, 10);	}; 
+$Content | ForEach-Object{ 
+	$digits = $_ -Split '\s+' | ForEach-Object { [convert]::ToInt32($_, 10); }; 
 	for($x=0;$x -lt $digits.length;$x++) {
 		for($y=0;$y -lt $digits.length;$y++) {
 			if($digits[$x]/ $digits[$y] -eq 1) { continue; }
@@ -47,4 +53,20 @@ $Content | %{
 	}
 }
 
-Write-Host $sum;
+Write-Host "Part 2 method 3: ${sum}";
+
+
+$sum=0;
+$Content | ForEach-Object{ 
+	$digits = [int[]]($_ -Split '\s+');
+	for($x=0;$x -lt $digits.length;$x++) {
+		for($y=0;$y -lt $digits.length;$y++) {
+			if($digits[$x]/ $digits[$y] -eq 1) { continue; }
+			if($digits[$x] % $digits[$y] -eq 0) {
+				$sum+=$digits[$x] / $digits[$y];
+			}
+		}
+	}
+}
+
+Write-Host "Part 2 method 4: ${sum}";
