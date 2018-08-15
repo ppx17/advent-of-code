e53fa8442a0add08e218c56c07fe069343e40b40
Niels Oostindiër <niels@bizqit.nl>
2018-08-15 16:07:05 +0200

    Add 2016 - day3

---
+++ 2016/day3.ps1
@@ -0,0 +1,27 @@
+Param(
+    [string]$InputFile = 'input-day3.txt'
+);
+
+$Lines = Get-Content $InputFile;
+
+$IntegerGrid = ($Lines | ForEach-Object { ,([int[]]@( ($_.TrimStart() -split "\s+"))) })
+
+$Triangles =  $IntegerGrid | Foreach-Object { ,([int[]]($_ | Sort-Object)); } ;
+
+$ValidTriangles = $Triangles | Where-Object { $_[0] + $_[1] -gt $_[2]; }
+
+Write-Output ("Part 1: {0}" -f ($ValidTriangles | Measure-Object).Count);
+
+$Triangles = [System.Collections.ArrayList]@();
+
+for($Y = 0; $Y -lt $IntegerGrid.Count; $Y+=3) {
+    for($X = 0; $X -lt $IntegerGrid[0].Count; $X++) {
+        [void]$Triangles.Add(
+            ([int[]](@($IntegerGrid[$Y][$X], $IntegerGrid[$Y+1][$X], $IntegerGrid[$Y+2][$X]) | Sort-Object))
+        );
+    }
+}
+
+$ValidTriangles = $Triangles | Where-Object { $_[0] + $_[1] -gt $_[2]; }
+
+Write-Output ("Part 2: {0}" -f ($ValidTriangles | Measure-Object).Count);
