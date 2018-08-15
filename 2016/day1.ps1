c31a229573183c38fb58e3f35c825bd67f760f77
Niels Oostindiër <niels@bizqit.nl>
2018-08-15 15:20:10 +0200

    Add 2016 day 1 and day 2

---
+++ 2016/day1.ps1
@@ -0,0 +1,79 @@
+Param(
+    [string]$InputFile = 'input-day1.txt'
+);
+
+$Instructions = ((Get-Content $InputFile -Raw) -Split ", ");
+
+# Navigator from 2017 - day22
+class Navigator {
+    [int]$X = 0;
+    [int]$Y = 0;
+    [int]$DirX = 0;
+    [int]$DirY = -1; 
+
+    Navigator($X, $Y) {
+        $this.X = $X;
+        $this.Y = $Y;
+    }
+
+    [void]TurnLeft() {
+        if($this.DirX -eq 0) {
+            $this.DirX = $this.DirY;
+            $this.DirY = 0;
+        }else{
+            $this.DirY = -$this.DirX;
+            $this.DirX = 0;
+        }
+    }
+
+    [void]TurnRight() {
+        if($this.DirX -eq 0) {
+            $this.DirX = -$this.DirY;
+            $this.DirY = 0;
+        }else{
+            $this.DirY = $this.DirX;
+            $this.DirX = 0;
+        }
+    }
+
+    [string[]]Move([Int32]$Distance = 1) {
+        $History = [System.Collections.ArrayList]@();
+        for($i=0;$i -lt $Distance; $i++) {
+            $this.X += ($this.DirX);
+            $this.Y += ($this.DirY);
+            [void]$History.Add(("{0}:{1}" -f $this.X, $this.Y));
+        }
+        return $History;
+    }
+}
+
+$Navigator = [Navigator]::new(0, 0);
+
+$LocationHistory = [System.Collections.ArrayList]@();
+$BunnyHQ = $null;
+
+Foreach($Instruction in $Instructions) {
+    $Dir = $Instruction[0];
+    $Distance = ([Int32]$Instruction.Substring(1));
+
+    if($Dir -eq "R") {
+        $Navigator.TurnRight();
+    }else{
+        $Navigator.TurnLeft();
+    }
+
+    $MoveHistory = $Navigator.Move($Distance);
+
+    if($null -eq $BunnyHQ) {
+        $MoveHistory | ForEach-Object {
+            if($_ -in $LocationHistory) {
+                $BunnyHQ = $_;
+            }
+            [void]$LocationHistory.Add($_);
+        }
+    }
+
+}
+
+Write-Output ("Part 1: {0}" -f ([Math]::Abs($Navigator.X) + [Math]::Abs($Navigator.Y)));
+Write-Output "Part 2: BunnyHQ is at ${BunnyHQ}";
---
+++ 2016/day2.ps1
@@ -0,0 +1,84 @@
+Param(
+    [string]$InputFile = 'input-day2.txt'
+);
+
+class Navigator {
+    [int]$X = 0;
+    [int]$Y = 0;
+
+    [char[][]]$Map;
+
+    Navigator([Int32]$X, [Int32]$Y, [char[][]]$Map) {
+        $this.X = $X;
+        $this.Y = $Y;
+        $this.Map = $Map;
+    }
+
+    [void]Move($Dir) {
+        switch($Dir) {
+            "U" {
+                if($this.ValidCoordinate($this.X, $this.Y - 1)) {
+                    $this.Y--;
+                }
+            }
+            "D" {
+                if($this.ValidCoordinate($this.X, $this.Y + 1)) {
+                    $this.Y++;
+                }
+            }
+            "L" { 
+                if($this.ValidCoordinate($this.X - 1, $this.Y)) {
+                    $this.X--;
+                }
+            }
+            "R" {
+                if($this.ValidCoordinate($this.X + 1, $this.Y)) {
+                    $this.X++;
+                }
+            }
+        }
+    }
+
+    [bool]ValidCoordinate($X, $Y) {
+        return $X -ge 0 `
+          -and $Y -ge 0 `
+          -and $X -lt $this.Map[0].Length `
+          -and $Y -lt $this.Map.Length `
+          -and $this.Map[$Y][$X] -ne ' ';
+    }
+
+    [string]CurrentKey() {
+        return $this.Map[$this.Y][$this.X];
+    }
+}
+
+$Navigators = @();
+
+$Navigators += [Navigator]::new(1, 1, @(
+    @('1','2','3'),
+    @('4','5','6'),
+    @('7','8','9')
+));
+
+$Navigators += [Navigator]::new(1, 2, @(
+    @(' ',' ','1',' ',' '),
+    @(' ','2','3','4',' '),
+    @('5','6','7','8','9'),
+    @(' ','A','B','C',' '),
+    @(' ',' ','D',' ',' ')
+));
+
+$Directions = Get-Content $InputFile;
+
+$Part = 1;
+foreach($Navigator in $Navigators) {
+    $Keys = @();
+    foreach($Line in $Directions) {
+        foreach($KeyPress in $Line.ToCharArray()) {
+            $Navigator.Move($KeyPress);
+        }
+        $Keys += $Navigator.CurrentKey();
+    }
+
+    Write-Output ("Part {0}: {1}" -f $Part++,($Keys -Join ""));
+}
