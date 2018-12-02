cb1bc939e9abb3b9a861ad69c9589698f3180779
Niels Oostindiër <niels@oostindier.eu>
2018-12-02 16:25:05 +0100

    Add 2018 day1 and day2 in php

---
+++ 2018/php/day2.php
@@ -0,0 +1,47 @@
+<?php
+$InputFile = "../input/input-day2.txt";
+
+$ids = explode("\n", trim(file_get_contents($InputFile)));
+
+function part1(array $ids): int
+{
+    $twos = $threes = 0;
+    foreach ($ids as $id) {
+        $counts = [];
+        for ($i = 0; $i < strlen($id); $i++) {
+            $counts[$id[$i]]++;
+        }
+        if (in_array(2, $counts)) {
+            $twos++;
+        }
+        if (in_array(3, $counts)) {
+            $threes++;
+        }
+    }
+
+    return $twos * $threes;
+}
+
+function common($first, $second): string
+{
+    $result = '';
+    for ($i = 0; $i < strlen($first); $i++) {
+        $result .= ($first[$i] === $second[$i]) ? $first[$i] : '';
+    }
+    return $result;
+}
+
+function part2($ids): ?string
+{
+    for ($fi = 0; $fi < count($ids); $fi++) {
+        for ($si = $fi + 1; $si < count($ids); $si++) {
+            if (levenshtein($ids[$fi], $ids[$si]) === 1) {
+                return common($ids[$fi], $ids[$si]);
+            }
+        }
+    }
+    return null;
+}
+
+echo "part1: " . part1($ids) . PHP_EOL;
+echo "part2: " . part2($ids);
