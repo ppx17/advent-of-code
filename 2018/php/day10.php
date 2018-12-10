e53c0756fd6e870a6387c347a98785589b4300c8
Niels <niels@bizqit.nl>
2018-12-10 09:33:44 +0100

    Optimized day10 for speed (376ms -> 192ms)

--- 2018/php/day10.php
+++ 2018/php/day10.php
@@ -1,102 +1,51 @@
 <?php
 $data = $data ?? file_get_contents("../input/input-" . basename(__FILE__, '.php') . ".txt");
 
-class Point
-{
-    public $x;
-    public $y;
-
-    public function __construct(int $x, int $y)
-    {
-        $this->x = $x;
-        $this->y = $y;
-    }
-
-    public function add(Point $point)
-    {
-        $this->x += $point->x;
-        $this->y += $point->y;
-    }
-}
-
-class Light
-{
-    public $position;
-    public $velocity;
-
-    public function __construct(Point $position, Point $velocity)
-    {
-        $this->position = $position;
-        $this->velocity = $velocity;
-    }
-
-    public function move()
-    {
-        $this->position->add($this->velocity);
-    }
-}
-
 class Sky
 {
-    private $lights;
+    private $positionsX = [];
+    private $positionsY = [];
+    private $velocitiesX = [];
+    private $velocitiesY = [];
+    private $lights = 0;
 
-    public function __construct()
+    public function addLight(int $posX, int $posY, int $velX, int $velY): void
     {
-        $this->lights = [];
-    }
-
-    public function addLight(Light $light): void
-    {
-        $this->lights[] = $light;
+        $this->positionsX[] = $posX;
+        $this->positionsY[] = $posY;
+        $this->velocitiesX[] = $velX;
+        $this->velocitiesY[] = $velY;
+        $this->lights++;
     }
 
     public function move(): void
     {
-        foreach ($this->lights as $light) {
-            $light->move();
+        for ($i = 0; $i < $this->lights; $i++) {
+            $this->positionsX[$i] += $this->velocitiesX[$i];
+            $this->positionsY[$i] += $this->velocitiesY[$i];
         }
     }
 
-    public function print()
+    public function height(): int
     {
-        $xs = $this->getXCoordinates();
-        $ys = $this->getYCoordinates();
-        $topLeft = new Point(min($xs), min($ys));
-        $bottomRight = new Point(max($xs), max($ys));
+        return max($this->positionsY) - min($this->positionsY);
+    }
 
+    public function print(): string
+    {
         $sky = [];
-        for ($y = $topLeft->y; $y <= $bottomRight->y; $y++) {
-            for ($x = $topLeft->x; $x <= $bottomRight->x; $x++) {
+        for ($y = min($this->positionsY); $y <= max($this->positionsY); $y++) {
+            for ($x = min($this->positionsX); $x <= max($this->positionsX); $x++) {
                 $sky[$y][$x] = ".";
             }
         }
-        foreach ($this->lights as $light) {
-            $sky[$light->position->y][$light->position->x] = '#';
+        for($i=0;$i<$this->lights;$i++) {
+            $sky[$this->positionsY[$i]][$this->positionsX[$i]] = '#';
         }
 
         return implode(PHP_EOL, array_map(function ($row) {
                 return implode("", $row);
             }, $sky)) . PHP_EOL;
-    }
-
-    public function height(): int
-    {
-        $ys = $this->getYCoordinates();
-        return max($ys) - min($ys);
-    }
-
-    private function getXCoordinates(): array
-    {
-        return array_map(function ($light) {
-            return $light->position->x;
-        }, $this->lights);
-    }
-
-    private function getYCoordinates(): array
-    {
-        return array_map(function ($light) {
-            return $light->position->y;
-        }, $this->lights);
     }
 }
 
@@ -106,24 +55,20 @@ preg_match_all(
     $data,
     $matches, PREG_SET_ORDER);
 
+;
 $sky = new Sky();
 
 foreach ($matches as $match) {
-    $sky->addLight(
-        new Light(
-            new Point(intval($match['px']), intval($match['py'])),
-            new Point(intval($match['vx']), intval($match['vy']))
-        )
-    );
+    $sky->addLight($match['px'], $match['py'], $match['vx'], $match['vy']);
 }
 
 $iterations = 0;
 do {
     $sky->move();
     $iterations++;
-} while ($sky->height() > 15);
+} while ($sky->height() > 10);
 
 
 echo "Part 1:" . PHP_EOL;
 echo $sky->print();
-echo "Part 2: " . $iterations;
+echo "Part 2: " . $iterations.PHP_EOL;
