fc54d05fe6bfecda7dce433d94542ee471847f96
niels <info@codelink.nl>
2019-12-01 11:36:04 +0100

    Update CI

---
+++ 2019/php/src/Aoc/Days/Day2.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Ppx17\Aoc2019\Aoc\Days;
+
+
+class Day2 extends AbstractDay
+{
+
+    public function dayNumber(): int
+    {
+        return 2;
+    }
+
+    public function part1(): string
+    {
+        return '';
+    }
+
+    public function part2(): string
+    {
+
+        return '';
+    }
+}
