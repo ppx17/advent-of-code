5b99be089144c9b0177266fbd1d7db88b1ae9339
Niels Oostindiër <niels@oostindier.eu>
2020-10-24 13:46:09 +0200

    Fix day5 intcode input

--- 2019/julia/src/day5.jl
+++ 2019/julia/src/day5.jl
@@ -8,14 +8,14 @@ intcode = Aoc.intcode(5)
 
 part1 = Computer(copy(intcode))
 
-part1.input = 1
+part1.input = [1]
 Intcode.run!(part1)
 
 println("Part 1: ", part1.output)
 
 part2 = Computer(copy(intcode))
 
-part2.input = 5
+part2.input = [5]
 Intcode.run!(part2)
 
 println("Part 2: ", part2.output)
