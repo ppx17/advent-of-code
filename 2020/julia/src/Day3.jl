620ac0126a178a336987290988766e8cf3da6af7
Niels Oostindiër <niels@oostindier.eu>
2020-12-03 07:55:51 +0100

    Add 2020 day3 in Julia

---
+++ 2020/julia/src/Day3.jl
@@ -0,0 +1,28 @@
+module Day3
+
+include("Aoc.jl")
+using .Aoc
+
+map = Aoc.input_lines(3)
+
+width = length(map[1])
+height = size(map)[1]
+
+isTree(pos) = map[pos[2]][((pos[1]-1) % width) + 1] == '#'
+
+function treeCount(direction)
+    pos = [1, 1]
+    trees = 0
+
+    while pos[2] <= height
+        trees += isTree(pos) ? 1 : 0
+        pos += direction
+    end
+    
+    trees
+end
+
+part1() = treeCount([3, 1])
+part2() = mapfoldr(treeCount, *, eachrow([ 1 1; 3 1; 5 1; 7 1; 1 2 ]))
+
+end
