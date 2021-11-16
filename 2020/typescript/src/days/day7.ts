23b9f68d2ae825fae707c80b25e1a72ee3cab1b1
Niels <niels@oostindier.eu>
2021-11-16 17:23:30 +0100

    Add 2020/day7.ts

---
+++ 2020/typescript/src/days/day7.ts
@@ -0,0 +1,52 @@
+import {Day} from "../aoc/day";
+import {sum} from "./helpers";
+
+export class Day7 extends Day {
+    private bags = new Map<Color, Bag>();
+
+    day = (): number => 7;
+
+    part1 = (): string => this.countContainers('shiny gold').toString();
+
+    part2 = (): string => this.countBagsRecursive('shiny gold').toString();
+
+    setup() {
+        super.setup();
+
+        this.input
+            .map(l => l.match(/^(?<bag>\w+ \w+) bags contain (?<content>(\d+ \w+ \w+ bags?,? ?)+)\.$/))
+            .filter(r => r)
+            .map((r): Bag => ({color: r.groups.bag, content: Day7.parseContent(r.groups.content)}))
+            .forEach(b => this.bags.set(b.color, b));
+    }
+
+    private static parseContent(content: string): ContentLine[] {
+        return content.split(',')
+            .map(c => c.match(/(?<num>\d+) (?<color>\w+ \w+) bags?/))
+            .map((m): ContentLine => ({color: m.groups.color, amount: Number(m.groups.num)}));
+    }
+
+    private countContainers(color: Color, seen: Set<Color> = new Set<Color>()): number {
+        this.bags.forEach((bag) => {
+            if (bag.content.filter(l => l.color === color).length > 0) {
+                seen.add(bag.color);
+                this.countContainers(bag.color, seen);
+            }
+        });
+
+        return seen.size;
+    }
+
+    private countBagsRecursive(color: Color): number {
+        return !this.bags.has(color)
+            ? 0
+            : this.bags.get(color)
+                .content
+                .map(line => line.amount + (line.amount * this.countBagsRecursive(line.color)))
+                .reduce(sum);
+    }
+}
+
+type Color = string;
+type ContentLine = { color: Color, amount: number };
+type Bag = { color: Color, content: ContentLine[] };
--- 2020/typescript/src/days/index.ts
+++ 2020/typescript/src/days/index.ts
@@ -1,10 +1,10 @@
 export * from "./day1"
 export * from "./day2"
 export * from "./day3"
-
+export * from "./day4"
 export * from "./day5"
 export * from "./day6"
-
+export * from "./day7"
 export * from "./day8"
 export * from "./day9"
 export * from "./day10"
