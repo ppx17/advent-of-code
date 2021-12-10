e39d4faca93e684640aad740df13786353f870db
Niels Oostindiër <niels@oostindier.eu>
2021-12-10 16:11:45 +0100

    Clean-up in 2021/day10.ts

--- 2021/typescript/src/days/day10.ts
+++ 2021/typescript/src/days/day10.ts
@@ -3,24 +3,25 @@ import {Day, sum} from "../aoc";
 export class Day10 extends Day {
     day = (): number => 10;
 
+    private brackets: Map<string, string> = new Map([["{", "}"], ["(", ")"], ["<", ">"], ["[", "]"]]);
+    private lines: string[][];
+
     part1 = () => this.process(c => ({")": 3, "]": 57, "}": 1197, ">": 25137})[c] ?? 0).reduce(sum)
 
     part2 = () => Day10.median(this.process(
-            _ => 0,
-            queue => queue.reverse().reduce((acc, c) => acc * 5 + {"(": 1, "[": 2, "{": 3, "<": 4}[c], 0)
-        ).filter(s => s > 0));
+        _ => 0,
+        queue => queue.reverse().reduce((acc, c) => acc * 5 + ["(", "[", "{", "<"].indexOf(c) + 1, 0)
+    ).filter(s => s > 0));
+
+    setup = () => this.lines = this.input.map(l => l.split(''));
 
     private process = (corrupted: (c: string) => number, incomplete?: (queue: string[]) => number): number[] =>
         this.lines.map(line => {
-            const queue = [];
+            const queue: string[] = [];
 
             for (const c of line) {
-                if (["(", "[", "{", "<"].indexOf(c) !== -1) {
-                    queue.push(c);
-                } else {
-                    const toClose = queue.pop();
-                    if ({"{": "}", "(": ")", "<": ">", "[": "]"}[toClose] !== c) return corrupted(c);
-                }
+                if (this.brackets.has(c)) queue.push(c);
+                else if (this.brackets.get(queue.pop()) !== c) return corrupted(c);
             }
 
             return incomplete === undefined ? 0 : incomplete(queue);
@@ -28,8 +29,4 @@ export class Day10 extends Day {
 
     private static median = (numbers: number[]): number =>
         numbers.sort((a, b) => a - b)[Math.floor(numbers.length / 2)];
-
-    setup = () => this.lines = this.input.map(l => l.split(''));
-
-    private lines: string[][];
 }
