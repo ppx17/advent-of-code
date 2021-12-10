91e7adb56bd3f4e7ef375d737a85251a33c4f066
Niels <niels@oostindier.eu>
2021-12-10 06:53:02 +0100

    Add day template.

---
+++ 2021/typescript/src/days/_template.ts
@@ -0,0 +1,18 @@
+import {Day} from "../aoc";
+
+export class Dayx extends Day {
+    day = (): number => ;
+
+    part1 = () => {
+
+        return '';
+    }
+    part2 = () => {
+
+        return '';
+    }
+
+    setup = () => {
+
+    };
+}
