6a3738913c5800c74f2edb02a66aaeaec1a983f4
Niels <niels@oostindier.eu>
2021-12-18 19:53:47 +0100

    Add 2021/day18.ts

--- 2021/typescript/src/days/day18.ts
+++ 2021/typescript/src/days/day18.ts
@@ -16,20 +16,15 @@ export class Day18 extends Day {
         .flat()
         .reduce((a, b) => a > b ? a : b);
 
-    setup = () => {
-        this.calculator = new Calculator();
-        this.parsed = this.input.map(l => Parser.parseLine(l));
-    };
+    setup = () => this.calculator = new Calculator();
 
     private calculator: Calculator;
-    private parsed: Pair[];
 }
 
 export class Parser {
     public static parseLine(l: string): Pair {
-        let level = 0;
+        let level = 0, last: Pair | undefined;
         const pairs: Pair[] = [];
-        let last: Pair | undefined;
 
         for (let i = 0; i < l.length; i++) {
             if (l[i] === '[') {
@@ -38,11 +33,9 @@ export class Parser {
                 if (pairs.length > 0) {
                     const parent = pairs[pairs.length - 1];
                     pair.parent = parent;
-                    if (parent.left === undefined) {
-                        parent.left = pair;
-                    } else if (parent.right === undefined) {
-                        parent.right = pair;
-                    }
+                    parent.left === undefined
+                        ? parent.left = pair
+                        : parent.right = pair;
                 }
                 pairs.push(pair);
             } else if (l[i] === ']') {
@@ -50,14 +43,11 @@ export class Parser {
                 last = pairs.pop();
             } else if (l[i].match(/[0-9]/)) {
                 let n = l[i];
-                if ((l[i + 1] ?? '').match(/[0-9]/)) n += l[++i];
+                while ((l[i + 1] ?? '').match(/[0-9]/)) n += l[++i];
 
                 const parent = pairs[pairs.length - 1];
-                if (parent.left === undefined) {
-                    parent.left = new Scalar(level + 1, Number(n), parent);
-                } else if (parent.right === undefined) {
-                    parent.right = new Scalar(level + 1, Number(n), parent);
-                }
+                const scalar = new Scalar(level + 1, Number(n), parent)
+                parent.left === undefined ? parent.left = scalar : parent.right = scalar;
             }
         }
 
@@ -81,7 +71,7 @@ export class Calculator {
 
         const reduced = this.reduce(result);
 
-        if (reduced instanceof Scalar) throw "Reduce should never return a scalar as end result.";
+        if (reduced.isScalar()) throw "Reduce should never return a scalar as end result.";
 
         return reduced;
     }
@@ -89,65 +79,29 @@ export class Calculator {
     public reduce(pair: Scalar | Pair): Scalar | Pair {
         this.actionExecuted = true;
         while (this.actionExecuted) {
-            console.log(pair.toString());
             this.actionExecuted = false;
             pair = this.explode(pair);
             if (this.actionExecuted) continue;
 
             pair = this.split(pair);
-
-            console.log(this.actionExecuted);
         }
 
         return pair;
     }
 
     public explode(pair: Pair | Scalar): Scalar | Pair {
-        if (pair instanceof Scalar) return pair;
+        if (pair.isScalar()) return pair;
 
-        if (pair.level > 4 && pair.left instanceof Scalar && pair.right instanceof Scalar) {
-
-            let p = pair.parent;
-            let child = pair;
-
-            while (p !== undefined) {
-                if (p.left instanceof Scalar) {
-                    p.left.addToRight(pair.left.value);
-                    break;
-                } else {
-                    if (p.left === child) {
-                        child = p;
-                        p = p.parent;
-                    } else {
-                        p.left.addToRight(pair.left.value);
-                        break;
-                    }
-                }
-            }
-
-            p = pair.parent;
-            child = pair;
-            while (p !== undefined) {
-                if (p.right instanceof Scalar) {
-                    p.right.addToLeft(pair.right.value);
-                    break;
-                } else {
-                    if (p.right === child) {
-                        child = p;
-                        p = p.parent;
-                    } else {
-                        p.right.addToLeft(pair.right.value);
-                        break;
-                    }
-                }
-            }
+        if (pair.shouldExplode() && pair.left.isScalar() && pair.right.isScalar()) {
+            pair.addToParentOnLeft(pair.left.value);
+            pair.addToParentOnRight(pair.right.value);
 
             this.actionExecuted = true;
             return new Scalar(pair.level, 0, pair.parent);
         }
 
 
-        if (!this.actionExecuted) pair.left = this.explode(pair.left);
+        pair.left = this.explode(pair.left);
         if (!this.actionExecuted) pair.right = this.explode(pair.right);
 
         return pair;
@@ -156,20 +110,23 @@ export class Calculator {
     public split(pair: Pair | Scalar): Pair | Scalar {
         if (pair instanceof Scalar) return pair;
 
-        if (pair.left instanceof Scalar && pair.left.shouldSplit()) {
+        if (pair.left.isScalar() && pair.left.shouldSplit()) {
             this.actionExecuted = true;
             pair.left = pair.left.split();
             return pair;
         }
 
-        if (!this.actionExecuted) pair.left = this.split(pair.left);
+        pair.left = this.split(pair.left);
 
-        if (!this.actionExecuted && pair.right instanceof Scalar && pair.right.shouldSplit()) {
+        if (this.actionExecuted) return pair;
+
+        if (pair.right.isScalar() && pair.right.shouldSplit()) {
             this.actionExecuted = true;
             pair.right = pair.right.split();
             return pair;
         }
-        if (!this.actionExecuted) pair.right = this.split(pair.right);
+
+        pair.right = this.split(pair.right);
         return pair;
     }
 }
@@ -178,6 +135,12 @@ abstract class Data {
     parent?: Pair;
 
     constructor(public level: number) {
+    }
+
+    isScalar = (): this is Scalar => this instanceof Scalar;
+
+    levelUp() {
+        this.level++;
     }
 
     abstract toString(): string;
@@ -190,14 +153,31 @@ export class Pair extends Data {
     toString = (): string => `[${this.left.toString()},${this.right.toString()}]`;
 
     levelUp() {
+        this.level++;
         this.left?.levelUp();
         this.right?.levelUp();
-        this.level++;
+    }
+
+    addToParentOnLeft = (inc: number) => {
+        if (this.parent === undefined) return;
+
+        this.parent.left === this
+            ? this.parent.addToParentOnLeft(inc)
+            : this.parent.left.addToRight(inc);
+    }
+
+    addToParentOnRight = (inc: number) => {
+        if (this.parent === undefined) return;
+
+        this.parent.right === this
+            ? this.parent.addToParentOnRight(inc)
+            : this.parent.right.addToLeft(inc);
     }
 
     addToLeft = (inc: number) => this.left.addToLeft(inc);
-
     addToRight = (inc: number) => this.right.addToRight(inc);
+
+    shouldExplode = () => this.level > 4;
 
     magnitude = (): number => 3 * this.left.magnitude() + 2 * this.right.magnitude();
 }
@@ -208,14 +188,11 @@ export class Scalar extends Data {
         this.parent = parent;
     }
 
-    magnitude(): number {
-        return this.value;
-    }
+    magnitude = (): number => this.value;
 
-    levelUp = () => this.level++;
-
-    addToLeft = (inc: number) => this.value += inc;
-    addToRight = (inc: number) => this.value += inc;
+    increment = (inc: number) => this.value += inc;
+    addToLeft = this.increment;
+    addToRight = this.increment;
 
     split(): Pair {
         const pair = new Pair(this.level);
