adc1e4c6b513ab8643336171e5712f9dcd8a1029
Niels <niels@oostindier.eu>
2021-12-20 14:46:01 +0100

    Refactor and optimize 2021/day19.ts a bit.

--- 2021/typescript/src/days/day19.ts
+++ 2021/typescript/src/days/day19.ts
@@ -1,16 +1,16 @@
-import {Day, Vector} from "../aoc";
+import {Day, Vec, Vector} from "../aoc";
 
 export class Day19 extends Day {
     day = (): number => 19;
 
-    part1 = () => new Set<string>(this.fixedScanners.map(s => s.beacons.map(b => b.serialize())).flat()).size;
-    part2 = () => Math.max(...this.scannerPositions.map(a => Math.max(...this.scannerPositions.map(b => a.manhattan(b)))))
+    part1 = () => new Set<string>(this.knownScanners.map(s => s.beacons.map(b => b.serialize())).flat()).size;
+    part2 = () => Math.max(...this.knownScanners.map(a => Math.max(...this.knownScanners.map(b => a.position.manhattan(b.position)))))
 
     setup = () => {
         let activeScanner: Scanner;
         this.input.forEach(l => {
             if (l.startsWith('---')) {
-                if (activeScanner !== undefined) this.scanners.push(activeScanner);
+                if (activeScanner !== undefined) this.allScanners.push(activeScanner);
                 activeScanner = new Scanner(l);
             }
             if (l !== "") {
@@ -19,111 +19,136 @@ export class Day19 extends Day {
                 activeScanner?.beacons.push(v);
             }
         })
-        if (activeScanner !== undefined) this.scanners.push(activeScanner);
+        if (activeScanner !== undefined) this.allScanners.push(activeScanner);
 
-        let unknownScanners = [...this.scanners.slice(1)];
-        this.fixedScanners = [this.scanners[0]];
+        let unknownScanners = [...this.allScanners.slice(1)];
+        this.knownScanners = [this.allScanners[0]];
 
-        let foundScanner = true;
-
-        while (foundScanner && unknownScanners.length > 0) {
-            foundScanner = false;
-            for (const scannerA of this.fixedScanners) {
-                for (const scannerB of unknownScanners) {
-                    let offset = this.findOffset(scannerA, scannerB);
+        let found = true;
+        while (found && unknownScanners.length > 0) {
+            found = false;
+            for (const known of this.knownScanners) {
+                for (const unknown of unknownScanners) {
+                    let offset = this.findOffset(known, unknown);
 
                     if (offset === undefined) continue;
 
-                    this.scannerPositions.push(offset.offset);
-
-                    this.fixedScanners.push(new Scanner(scannerB.name, scannerB.beacons.map(b => offset.rotation.apply(b).add(offset.offset))));
-                    unknownScanners = unknownScanners.filter(s => s !== scannerB);
-                    foundScanner = true;
+                    this.knownScanners.push(unknown.applyOffset(offset));
+                    unknownScanners = unknownScanners.filter(s => s !== unknown);
+                    found = true;
                 }
             }
         }
     }
 
-    private findOffset = (scannerA: Scanner, scannerB: Scanner): Offset | undefined => {
-        for (const rotation of Rotation.all) {
+    private findOffset = (known: Scanner, unknown: Scanner): Offset | undefined => {
+        const target = 12;
+        for (const rotation of QuickRotation.all()) {
             const offsets = new Map<string, number>();
+            let high = 0;
+            let todo = known.beacons.length;
 
-            for (const beaconA of scannerA.beacons) {
-
-                for (const beaconB of scannerB.beacons) {
-                    const rotatedBeaconB = rotation.apply(beaconB);
-
-                    const offset = beaconA.sub(rotatedBeaconB);
+            for (const beaconA of known.beacons) {
+                for (const beaconB of unknown.beacons) {
+                    const offset = beaconA.sub(rotation.apply(beaconB));
 
                     const k = offset.serialize();
                     const c = (offsets.get(k) ?? 0) + 1;
+                    if (c === target) return {rotation, offset};
+
                     offsets.set(k, c);
-                    if (c >= 12) {
-                        return {rotation, offset};
-                    }
+                    high = Math.max(high, c);
                 }
+
+                if(--todo < target - high) break;
             }
         }
 
         return undefined;
     };
 
-    private fixedScanners: Scanner[];
-    private scannerPositions: Vector[] = [];
-    private scanners: Scanner[] = [];
+    private knownScanners: Scanner[];
+    private allScanners: Scanner[] = [];
 }
 
 type Offset = {
-    offset: Vector;
+    offset: Vec;
     rotation: Rotation
 }
 
 class Scanner {
-    constructor(public name: string, public beacons: Vector[] = []) {
+    constructor(public name: string, public beacons: Vec[] = [], public position: Vector = Vector.zero(3)) {
+    }
+
+    applyOffset = (offset: Offset): Scanner => {
+        this.position = offset.offset;
+        this.beacons = this.beacons.map(b => offset.rotation.apply(b).add(offset.offset));
+        return this;
     }
 }
 
-class Rotation {
-    public static all: Rotation[] = [
-        new Rotation(0, 0, 0),
-        new Rotation(1, 0, 0),
-        new Rotation(2, 0, 0),
-        new Rotation(3, 0, 0),
+interface Rotation {
+    apply(vec: Vec): Vec;
+}
 
-        new Rotation(0, 1, 0),
-        new Rotation(1, 1, 0),
-        new Rotation(2, 1, 0),
-        new Rotation(3, 1, 0),
+class SteppedRotation {
+    public static* all(): Generator<Rotation> {
+        for (let y = 0; y < 4; y++)
+            for (let x = 0; x < 4; x++)
+                yield new SteppedRotation(x, y, 0);
 
-        new Rotation(0, 2, 0),
-        new Rotation(1, 2, 0),
-        new Rotation(2, 2, 0),
-        new Rotation(3, 2, 0),
-
-        new Rotation(0, 3, 0),
-        new Rotation(1, 3, 0),
-        new Rotation(2, 3, 0),
-        new Rotation(3, 3, 0),
-
-        new Rotation(0, 0, 1),
-        new Rotation(1, 0, 1),
-        new Rotation(2, 0, 1),
-        new Rotation(3, 0, 1),
-
-        new Rotation(0, 0, 3),
-        new Rotation(1, 0, 3),
-        new Rotation(2, 0, 3),
-        new Rotation(3, 0, 3),
-    ];
+        for (let z of [1, 3])
+            for (let x = 0; x < 4; x++)
+                yield new SteppedRotation(x, 0, z);
+    }
 
     constructor(public x: number, public y: number, public z: number) {
     }
 
-    apply(vec: Vector): Vector {
+    apply(vec: Vec): Vec {
         for (let z = 0; z < this.z; z++) vec = vec.rotateAroundZ();
         for (let y = 0; y < this.y; y++) vec = vec.rotateAroundY();
         for (let x = 0; x < this.x; x++) vec = vec.rotateAroundX();
 
         return vec;
     }
+}
+
+class QuickRotation implements Rotation {
+    private static rotations: Rotation[];
+    public static all(): Rotation[] {
+        return this.rotations ??= [
+            new QuickRotation(v => v),
+            new QuickRotation(v => new Vector(v.x, -v.y, -v.z)),
+            new QuickRotation(v => new Vector(v.x, -v.z, v.y)),
+            new QuickRotation(v => new Vector(v.x, v.z, -v.y)),
+
+            new QuickRotation(v => new Vector(-v.x, v.y, -v.z)),
+            new QuickRotation(v => new Vector(-v.x, -v.y, v.z)),
+            new QuickRotation(v => new Vector(-v.x, v.z, v.y)),
+            new QuickRotation(v => new Vector(-v.x, -v.z, -v.y)),
+
+            new QuickRotation(v => new Vector(v.y, v.x, -v.z)),
+            new QuickRotation(v => new Vector(v.y, -v.x, v.z)),
+            new QuickRotation(v => new Vector(v.y, v.z, v.x)),
+            new QuickRotation(v => new Vector(v.y, -v.z, -v.x)),
+
+            new QuickRotation(v => new Vector(-v.y, v.x, v.z)),
+            new QuickRotation(v => new Vector(-v.y, -v.x, -v.z)),
+            new QuickRotation(v => new Vector(-v.y, v.z, -v.x)),
+            new QuickRotation(v => new Vector(-v.y, -v.z, v.x)),
+
+            new QuickRotation(v => new Vector(v.z, v.x, v.y)),
+            new QuickRotation(v => new Vector(v.z, -v.x, -v.y)),
+            new QuickRotation(v => new Vector(v.z, v.y, -v.x)),
+            new QuickRotation(v => new Vector(v.z, -v.y, v.x)),
+
+            new QuickRotation(v => new Vector(-v.z, v.x, -v.y)),
+            new QuickRotation(v => new Vector(-v.z, -v.x, v.y)),
+            new QuickRotation(v => new Vector(-v.z, v.y, v.x)),
+            new QuickRotation(v => new Vector(-v.z, -v.y, -v.x)),
+        ];
+    }
+
+    constructor(public apply: (vec: Vec) => Vec) {}
 }
--- lib/typescript/aoc/src/helpers.ts
+++ lib/typescript/aoc/src/helpers.ts
@@ -1,6 +1,129 @@
-export class Vector {
+export interface Vec {
+    readonly values: number[];
+
+    get x(): number;
+
+    get y(): number;
+
+    get z(): number;
+
+    get dimensions(): number;
+
+    between(a: Vec, b: Vec): boolean;
+
+    within(boundary: Vector): boolean;
+
+    manhattan(other: Vec): number;
+
+    is(other: Vec): boolean;
+
+    serialize(): string;
+
+    add(other: Vec): Vec;
+
+    sub(other: Vec): Vec;
+
+    times(t: number): Vec;
+
+    abs(): Vec;
+
+    rotate3dRightAroundDimension(d: number): Vec;
+
+    rotate3dLeftAroundDimension(d: number): Vec;
+
+    rotateAroundX(): Vec;
+
+    rotateAroundY(): Vec;
+
+    rotateAroundZ(): Vec;
+
+    mutable(): MutableVector;
+
+    fixed(): Vector;
+}
+
+abstract class BaseVector {
     public readonly values: number[];
 
+    constructor(...values: number[]) {
+        this.values = values;
+    }
+
+    get x(): number {
+        return this.values[0] ?? 0;
+    }
+
+    get y(): number {
+        return this.values[1] ?? 0;
+    }
+
+    get z(): number {
+        return this.values[2] ?? 0;
+    }
+
+    get dimensions(): number {
+        return this.values.length;
+    }
+
+    between(a: Vec, b: Vec): boolean {
+        if (this.dimensions > a.dimensions || this.dimensions > b.dimensions) return false;
+
+        for (let d = 0; d < this.dimensions; d++)
+            if (this.values[d] < Math.min(a.values[d], b.values[d])
+                || this.values[d] > Math.max(a.values[d], b.values[d]))
+                return false;
+
+        return true;
+    }
+
+    within(boundary: Vector): boolean {
+        for (let i = 0; i < boundary.dimensions; i++) {
+            const b = boundary.values[i];
+            if (b >= 0) {
+                if (this.values[i] < 0 || this.values[i] > b) {
+                    return false;
+                }
+            } else {
+                if (this.values[i] > 0 || this.values[i] < b) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    manhattan(other: Vec): number {
+        return this.values
+            .map((v: number, i: number) => Math.abs(v - other.values[i] ?? 0))
+            .reduce(sum);
+    }
+
+    abstract rotate3dLeftAroundDimension(d: number): Vec;
+    abstract rotate3dRightAroundDimension(d: number): Vec;
+
+    rotateAroundX(): Vec {
+        return this.rotate3dRightAroundDimension(0);
+    }
+
+    rotateAroundY(): Vec {
+        return this.rotate3dRightAroundDimension(1);
+    }
+    rotateAroundZ(): Vec {
+        return this.rotate3dRightAroundDimension(2);
+    }
+
+    is(other: Vec): boolean {
+        return this.values.length === other.values.length
+            && this.values.every((v, i) => v === other.values[i]);
+    }
+
+    serialize(): string {
+        return this.values.join(':');
+    }
+}
+
+export class Vector extends BaseVector implements Vec {
     public static zero(dimensions: number = 2): Vector {
         return new this(...Array.from({length: dimensions}).map(() => 0));
     }
@@ -25,104 +148,98 @@ export class Vector {
         return new this(-1, 0);
     }
 
-    constructor(...values: number[]) {
-        this.values = values;
+    add = (other: Vec): Vector => new Vector(...this.values.map((v, i) => v + other.values[i] ?? 0));
+
+    sub = (other: Vec): Vector => new Vector(...this.values.map((v, i) => v - other.values[i] ?? 0));
+
+    times = (t: number): Vector => new Vector(...this.values.map(v => v * t));
+
+    abs = (): Vector => new Vector(...this.values.map((v) => Math.abs(v)));
+
+    rotate3dRightAroundDimension(d: number): Vec {
+        const rotated = [];
+        const a = (d + 1) % 3;
+        const b = (d + 2) % 3;
+        rotated[d] = this.values[d];
+        rotated[a] = -this.values[b];
+        rotated[b] = this.values[a];
+        return new Vector(...rotated);
     }
 
-    get x(): number {
-        return this.values[0] ?? 0;
+    rotate3dLeftAroundDimension(d: number): Vec {
+        const rotated = [];
+        const a = (d + 1) % 3;
+        const b = (d + 2) % 3;
+        rotated[d] = this.values[d];
+        rotated[a] = this.values[b];
+        rotated[b] = -this.values[a];
+        return new Vector(...rotated);
     }
 
-    get y(): number {
-        return this.values[1] ?? 0;
-    }
-
-    get z(): number {
-        return this.values[2] ?? 0;
-    }
-
-    get dimensions(): number {
-        return this.values.length;
-    }
-
-    dimension(dimension: number): number {
-        return this.values[dimension];
-    }
-
-    add(other: Vector): Vector {
-        return new Vector(...this.values.map((v, i) => v + other.values[i] ?? 0));
-    }
-
-    sub(other: Vector): Vector {
-        return new Vector(...this.values.map((v, i) => v - other.values[i] ?? 0));
-    }
-
-    abs(): Vector {
-        return new Vector(...this.values.map((v) => Math.abs(v)));
-    }
-
-    within(boundary: Vector): boolean {
-        for (let i = 0; i < boundary.dimensions; i++) {
-            const b = boundary.values[i];
-            if (b >= 0) {
-                if (this.values[i] < 0 || this.values[i] > b) {
-                    return false;
-                }
-            } else {
-                if (this.values[i] > 0 || this.values[i] < b) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    rotateAroundZ(): Vector {
-        return new Vector(-this.y, this.x, this.z);
-    }
-
-    rotateAroundX(): Vector {
+    rotateAroundX(): Vec {
         return new Vector(this.x, -this.z, this.y);
     }
 
-    rotateAroundY(): Vector {
-        return new Vector(this.z, this.y, -this.x);
+    rotateAroundY(): Vec {
+        return new Vector(-this.z, this.y, this.x);
     }
-
-    between(a: Vector, b: Vector): boolean {
-        if (this.dimensions > a.dimensions || this.dimensions > b.dimensions) return false;
-
-        for (let d = 0; d < this.dimensions; d++)
-            if (this.values[d] < Math.min(a.values[d], b.values[d])
-                || this.values[d] > Math.max(a.values[d], b.values[d]))
-                return false;
-
-        return true;
-    }
-
-    times(t: number): Vector {
-        return new Vector(...this.values.map(v => v * t));
-    }
-
-    manhattan(other: Vector): number {
-        return this.values
-            .map((v: number, i: number) => Math.abs(v - other.values[i] ?? 0))
-            .reduce(sum);
-    }
-
-    public is(other: Vector): boolean {
-        return this.values.length === other.values.length
-            && this.values.every((v, i) => v === other.values[i]);
-    }
-
-    serialize(): string {
-        return this.values.join(':');
+    rotateAroundZ(): Vec {
+        return new Vector(-this.y, this.x, this.z);
     }
 
     static deserialize(key: string) {
         return new Vector(...key.split(':').map(Number));
     }
+
+    mutable = (): MutableVector => new MutableVector(...this.values);
+    fixed = (): Vector => this;
+}
+
+class MutableVector extends BaseVector implements Vec {
+    abs(): Vec {
+        for (let i = 0; i < this.dimensions; i++)
+            this.values[i] = Math.abs(this.values[i]);
+        return this;
+    }
+
+    add(other: Vec): MutableVector {
+        for (let i = 0; i < this.dimensions; i++)
+            this.values[i] += other.values[i] ?? 0;
+        return this;
+    }
+
+    sub(other: Vec): MutableVector {
+        for (let i = 0; i < this.dimensions; i++)
+            this.values[i] -= other.values[i] ?? 0;
+        return this;
+    }
+
+    times(t: number): MutableVector {
+        for (let i = 0; i < this.dimensions; i++)
+            this.values[i] *= t;
+        return this;
+    }
+
+    rotate3dLeftAroundDimension(d: number): Vec {
+        const a = (d + 1) % 3;
+        const b = (d + 2) % 3;
+        const keep = this.values[a];
+        this.values[a] = this.values[b];
+        this.values[b] = -keep;
+        return this;
+    }
+
+    rotate3dRightAroundDimension(d: number): Vec {
+        const a = (++d) % 3;
+        const b = (++d) % 3;
+        const keep = this.values[a];
+        this.values[a] = -this.values[b];
+        this.values[b] = keep;
+        return this;
+    }
+
+    fixed = (): Vector => new Vector(...this.values);
+    mutable = (): MutableVector => this;
 }
 
 export function sum(a: number, b: number): number;
