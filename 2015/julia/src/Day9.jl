880b7f7c3b25e4ff07b36bc6cf9e1451a8066b70
Niels <niels@oostindier.eu>
2021-12-29 21:49:34 +0100

    Actually commit the code

--- 2015/julia/src/Day9.jl
+++ 2015/julia/src/Day9.jl
@@ -1,10 +1,44 @@
-module Day1
+module Day9
 
 include("Aoc.jl")
 using .Aoc
 
-d = parse.(Int64, Aoc.input_lines(1))
-part1() = count(i -> i > 1 && d[i] > d[i-1], eachindex(d))
-part2() = count(i -> i > 3 && sum(d[i-2:i]) > sum(d[i-3:i-1]), eachindex(d))
+legs = [match(r"([a-zA-Z]+) to ([a-zA-Z]+) = ([0-9]+)", l) for l in Aoc.input_lines(9)]
+
+cities = unique(vcat(map(m -> m.captures[1], legs), map(m -> m.captures[2], legs)))
+cityindices = Set(1:length(cities))
+
+distances = zeros(Int64, length(cities), length(cities))
+
+cityidx(name) = findfirst(c -> c == name, cities)
+
+for leg in legs
+    srcidx = cityidx(leg.captures[1])
+    dstidx = cityidx(leg.captures[2])
+    distances[[CartesianIndex(srcidx,dstidx), CartesianIndex(dstidx,srcidx)]] .= parse(Int64, leg.captures[3])
+end
+
+struct Route
+    length::Int64
+    visited::Set{Int64}
+    last::Int64
+end
+
+select(selector, routes) = selector(r -> r.length, routes)
+
+function visit(route::Route, selector)
+    tovisit = setdiff(cityindices, route.visited)
+
+    isempty(tovisit) && return route
+
+    routes = [visit(Route(route.length + distances[CartesianIndex(route.last, i)], push!(copy(route.visited), i), i), selector) for i in tovisit]
+
+    routes[select(selector, routes)[2]]
+end
+
+run(selector) = select(selector, [visit(Route(0, Set([i]), i), selector) for i in cityindices])[1]
+
+part1() = run(findmin)
+part2() = run(findmax)
 
 end
---
+++ 2015/julia/test/test.jl
@@ -0,0 +1,50 @@
+push!(LOAD_PATH, "../src")
+
+using Test
+using Aoc
+using Printf
+
+days = vcat(1:1)
+
+@time using Day9
+
+function format_time(sec::Float64)
+    ms = sec * 1000
+    if ms < 1
+        return @sprintf("% 8.2f µs", ms * 1000)
+    end
+    if ms < 1000
+        return @sprintf("% 8.2f ms", ms)
+    end
+    return @sprintf("\x1b[31m% 8.2f ms\x1b[0m", ms)
+end
+   
+header = "│ Day │   │      Part 1 │      Part 2 │          Result 1 │         Result 2 │"
+
+println(repeat("─", length(header)))
+println(header)
+println(repeat("─", length(header)))
+has_invalids = false
+for day in 1:25
+    day_symbol = Symbol("Day", day)
+    if isdefined(Main, day_symbol)
+        day_module = getfield(Main, day_symbol)
+        p1time = @timed day_module.part1()
+        p2time = @timed day_module.part2()
+        valid = Aoc.validate(day, p1time.value, p2time.value)
+        !valid && (global has_invalids = true)
+        println(@sprintf("│ % 2s  │ %s │ %s │ %s │   % 15s │  % 15s │",
+            day,
+            valid ? "\x1b[32m✔\x1b[0m" : "\x1b[31mX\x1b[0m",
+            format_time(p1time.time),
+            format_time(p2time.time),
+            p1time[1],
+            p2time[1]
+        ))
+    end
+end
+println(repeat("─", length(header)))
+
+if has_invalids
+    exit(1)
+end
